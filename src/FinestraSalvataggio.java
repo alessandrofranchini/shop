82aa53a1d2ea5c2571aafeed6929c6a30fd41ccd
giuse <demon.2347@gmail.com>
2013-12-09 21:47:49 +0100

    sistemato il salvataggio file

--- src/FinestraSalvataggio.java
+++ src/FinestraSalvataggio.java
@@ -9,11 +9,13 @@ public class FinestraSalvataggio extends JFileChooser{
 	@Override
 	public void approveSelection(){
         File f = getSelectedFile();
+        String oldname = f.getAbsolutePath();
+        f = new File(oldname+".html");
         if(f.exists()){
             int result = JOptionPane.showConfirmDialog(this,"Il file esiste, vuoi sovrascriverlo?","Il file esiste",JOptionPane.YES_NO_OPTION);
             switch(result){
                 case JOptionPane.YES_OPTION:
-                    
+                	super.approveSelection();
                     return;
                 case JOptionPane.NO_OPTION:
                     return;
